7de8b4851b2884c9449b370d7b5015e8fb9875a3
Andrometta <106281787+Andrometta@users.noreply.github.com>
2022-10-05 14:19:53 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0xB6bd46621C3085b7882937A84016BE949B6FdD8C",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "Meta Bucks NFT Dashboard",
 
     // 4. Set your Covalent API Key
     "api_key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
@@ -16,7 +16,7 @@ export const CONFIG = {
     "timeseries_chart": true,
 
     // 6. (Optional) Set your banner image
-    "banner_picture": "",
+    "banner_picture": "https://i.seadn.io/gcs/files/e27199637cfbce333ed793705071e483.gif?auto=format&w=1920",
   },
 
   // Supported networks - just for your reference
